0ccb491223b2bafe9b0d88ca2780fecc7b9cb650
LeldeZ <lelde.zulge@grobinasskola.lv>
2023-11-24 10:40:21 +0200

    paš'vērtējums

---
+++ pašvērtējums.py
@@ -0,0 +1,4 @@
+# pārsvarā visu sapratu, centos izdarīt visu un iekļauties laikā.
+#domāju ka darbu izdariju diezgan labi, varbūt vēl kaut kas nedaudz jāpamācās
+#kā kļūdu sev pielieku to ka nepaņēmu līdzi kladi, līdz ar to nevarēju neko apskatīties.
+# kopumā bija labi.
